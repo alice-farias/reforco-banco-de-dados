9a1dfe12b740ddaee08f0e8ab6da62713847aa0d
alice farias <alice.farias204@gmail.com>
2025-03-17 21:36:07 -0300

    primeira versao site reforço banco de dados. apenas home parcialmente concluida

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active');
+  }
+
+  function mostrarResposta() {
+    const resposta = document.getElementById('resposta');
+    resposta.style.display = 'block';
+  }
